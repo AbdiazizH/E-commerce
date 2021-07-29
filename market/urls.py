4820052f847413f1e4fa67f9e195174164c69fa6
AbdiazizH <a.aziz69890@gmail.com>
2021-07-29 15:03:27 +0300

    create and update class category in models.py

---
+++ market/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'ecommerce'
+  
+urlpatterns = [ 
+    path('', views.all_products, name='all_products'),  
+    
+]
---
+++ market/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Category, Product
+
+
+def all_products(request):
+    product = Product.objects.all()
+    return render(request, 'all_products/home.html',)
+    
+
